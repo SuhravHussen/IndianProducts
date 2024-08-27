03d76b8182cd6eba9f0ca4f759fce3fd3530561e
SuhravHussen <suhravshan@gmail.com>
2024-08-27 14:53:45 +0600

    adminlogin test

--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -1,15 +1,10 @@
-"use client";
-
 import { Separator } from "@/components/ui/separator";
-import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Layout({ children }) {
-  const { user } = useUser();
-
   return (
     <div>
       <div className="min-h-[20vh] flex flex-col justify-center items-center">
-        <h1 className="text-2xl font-bold ">Admin Panel - {user?.email}</h1>
+        <h1 className="text-2xl font-bold ">Admin Panel</h1>
       </div>
       <Separator />
       {children}
