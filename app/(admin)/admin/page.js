ad13011e246222a98831cf03325d6bf858272ae8
SuhravHussen <suhravshan@gmail.com>
2024-08-27 13:33:16 +0600

    adminlogin auth restored

--- app/(admin)/admin/page.js
+++ app/(admin)/admin/page.js
@@ -9,75 +9,75 @@ import { Separator } from "@/components/ui/separator";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { useEffect, useState } from "react";
 
-export default function Page() {
-  const [categoryList, setCategoryList] = useState([]);
+// export default function Page() {
+//   const [categoryList, setCategoryList] = useState([]);
 
-  const getCategoryList = async () => {
-    try {
-      const res = await fetch("/api/admin/category");
-      const data = await res.json();
+//   const getCategoryList = async () => {
+//     try {
+//       const res = await fetch("/api/admin/category");
+//       const data = await res.json();
 
-      setCategoryList(data.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//       setCategoryList(data.data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  // get category list
-  useEffect(() => {
-    getCategoryList();
-  }, []);
+//   // get category list
+//   useEffect(() => {
+//     getCategoryList();
+//   }, []);
 
-  return (
-    <section className="flex flex-col justify-center mx-auto p-8 max-w-[760px] gap-10">
-      <AddProduct categoryList={categoryList} />
-      <Separator />
-      <AddCategory refreshCategoryList={getCategoryList} />
-      <Separator />
-      <EditProduct />
-      <Separator />
-      <EditCategory refreshCategoryList={getCategoryList} />
-    </section>
-  );
-}
+//   return (
+//     <section className="flex flex-col justify-center mx-auto p-8 max-w-[760px] gap-10">
+//       <AddProduct categoryList={categoryList} />
+//       <Separator />
+//       <AddCategory refreshCategoryList={getCategoryList} />
+//       <Separator />
+//       <EditProduct />
+//       <Separator />
+//       <EditCategory refreshCategoryList={getCategoryList} />
+//     </section>
+//   );
+// }
 
-// const AdminPage = withPageAuthRequired(
-//   async () => {
-//     const [categoryList, setCategoryList] = useState([]);
+const AdminPage = withPageAuthRequired(
+  async () => {
+    const [categoryList, setCategoryList] = useState([]);
 
-//     const getCategoryList = async () => {
-//       try {
-//         const res = await fetch("/api/admin/category");
-//         const data = await res.json();
+    const getCategoryList = async () => {
+      try {
+        const res = await fetch("/api/admin/category");
+        const data = await res.json();
 
-//         setCategoryList(data.data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
+        setCategoryList(data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-//     // get category list
-//     useEffect(() => {
-//       getCategoryList();
-//     }, []);
-//     return (
-//       <section className="flex flex-col justify-center mx-auto p-8 max-w-[760px] gap-10">
-//         <AddProduct categoryList={categoryList} />
-//         <Separator />
-//         <AddCategory refreshCategoryList={getCategoryList} />
-//         <Separator />
-//         <EditProduct />
-//         <Separator />
-//         <EditCategory refreshCategoryList={getCategoryList} />
-//         <Button className="w-full">
-//           <a href="/api/auth/logout">Logout</a>
-//         </Button>
-//       </section>
-//     );
-//   },
-//   {
-//     returnTo: "/admin",
-//   }
-// );
+    // get category list
+    useEffect(() => {
+      getCategoryList();
+    }, []);
+    return (
+      <section className="flex flex-col justify-center mx-auto p-8 max-w-[760px] gap-10">
+        <AddProduct categoryList={categoryList} />
+        <Separator />
+        <AddCategory refreshCategoryList={getCategoryList} />
+        <Separator />
+        <EditProduct />
+        <Separator />
+        <EditCategory refreshCategoryList={getCategoryList} />
+        <Button className="w-full">
+          <a href="/api/auth/logout">Logout</a>
+        </Button>
+      </section>
+    );
+  },
+  {
+    returnTo: "/admin",
+  }
+);
 
-// export default AdminPage;
+export default AdminPage;
